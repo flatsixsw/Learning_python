c79f94730cc6953813c15728c15107819cb72308
Esteban Gonzalez <rider919@outlook.com>
2025-09-28 18:59:47 -0300

    Add 07_ to master

---
+++ 07_compund_interest.py
@@ -0,0 +1,10 @@
+# Calculating compund interest
+
+principal = 10000.0   # starting principal
+rate = .05            # interest rate
+
+print ("Year %21s" % "Amount on deposit")
+
+for year in range (1, 11):
+    amount = principal * (1.0 + rate) ** year
+    print ("%4d%21.2f" % (year, amount ))
